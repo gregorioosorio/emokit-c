97de31fe48611de3f1c4d7175d0e6198fce13a47
Gregorio Osorio <gregorioosorio16687@gmail.com>
2023-04-26 15:02:54 -0500

    add read value in contact program

--- examples/contact/contact.c
+++ examples/contact/contact.c
@@ -56,7 +56,8 @@ int main(int argc, char **argv)
 		int err = emokit_read_data_timeout(d, 1000);
 		if(err > 0) {
 			c = emokit_get_next_frame(d);			
-			fprintf(stdout,"\033[H\033[2JPress CTRL+C to exit\n\nContact quality:\nF3  %4d\nFC6 %4d\nP7  %4d\nT8  %4d\nF7  %4d\nF8  %4d\nT7  %4d\nP8  %4d\nAF4 %4d\nF4  %4d\nAF3 %4d\nO2  %4d\nO1  %4d\nFC5 %4d",c.cq.F3, c.cq.FC6, c.cq.P7, c.cq.T8,c.cq.F7, c.cq.F8, c.cq.T7, c.cq.P8, c.cq.AF4, c.cq.F4, c.cq.AF3, c.cq.O2, c.cq.O1, c.cq.FC5);
+			fprintf(stdout,"\033[H\033[2JPress CTRL+C to exit\nF3  %4d Quality: %4d\nFC6 %4d Quality: %4d\nP7  %4d Quality: %4d\nT8  %4d Quality: %4d\nF7  %4d Quality: %4d\nF8  %4d Quality: %4d\nT7  %4d Quality: %4d\nP8  %4d Quality: %4d\nAF4 %4d Quality: %4d\nF4  %4d Quality: %4d\nAF3 %4d Quality: %4d\nO2  %4d Quality: %4d\nO1  %4d Quality: %4d\nFC5 %4d Quality: %4d",\
+			c.F3, c.cq.F3, c.FC6, c.cq.FC6, c.P7, c.cq.P7, c.T8, c.cq.T8, c.F7, c.cq.F7, c.F8, c.cq.F8, c.T7, c.cq.T7, c.P8, c.cq.P8, c.AF4, c.cq.AF4, c.F4, c.cq.F4, c.AF3, c.cq.AF3, c.O2, c.cq.O2, c.O1, c.cq.O1, c.FC5, c.cq.FC5);
 			fflush(stdout);
 		} else if(err == 0) {
 			fprintf(stderr, "Headset Timeout...\n");
